4be386c2522b9b2c0cbc6578c688a2c02d2fee10
razaq himawan <razaqhim@gmail.com>
2024-11-02 09:42:28 +0700

    File based routing

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import pluginVue from 'eslint-plugin-vue'
 import vueTsEslintConfig from '@vue/eslint-config-typescript'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import { rules } from 'eslint-plugin-vue'
 
 export default [
   {
@@ -11,6 +12,11 @@ export default [
   {
     name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+  {
+    rules: {
+      'vue/multi-word-component-names': 0,
+    },
   },
 
   ...pluginVue.configs['flat/strongly-recommended'],
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,6 +5,8 @@ import { RouterLink } from 'vue-router'
 <template>
   <div>
     <h1>Home Page</h1>
-    <RouterLink to="/projects">Go to projects</RouterLink>
+    <RouterLink :to="{ name: '/projects/[id]', params: { id: 1 } }"
+      >Go to projects 1</RouterLink
+    >
   </div>
 </template>
