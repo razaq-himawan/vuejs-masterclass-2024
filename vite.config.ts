d49a4ff82e172e48ce6822eb057ff37dec0eb57c
razaq himawan <razaqhim@gmail.com>
2024-11-02 22:45:40 +0700

    Adding UI Elegance with Style

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,29 @@
 import { fileURLToPath, URL } from 'node:url'
 import VueRouter from 'unplugin-vue-router/vite'
 
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [VueRouter(), vue()],
+  plugins: [
+    VueRouter(),
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (element) => element.startsWith('iconify-icon'),
+        },
+      },
+    }),
+  ],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
