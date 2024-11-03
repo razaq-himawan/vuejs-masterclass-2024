1956b3c6fa5b345d8865b8fb8ec6c2504c9bf5c5
razaq himawan <razaqhim@gmail.com>
2024-11-03 12:56:29 +0700

    Refactoring The Project Architecture for Scalability

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,60 +1,22 @@
 <script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient'
-import { h, ref } from 'vue'
-import type { Tables } from 'database/types'
-import type { ColumnDef } from '@tanstack/vue-table'
-import DataTable from '@/components/ui/data-table/DataTable.vue'
-import { RouterLink } from 'vue-router'
+import { projectsQuery } from '@/utils/supaQueries'
+import type { Projects } from '@/utils/supaQueries'
+import { columns } from '@/utils/tableColumns/projectsColumns'
 
-const projects = ref<Tables<'projects'>[] | null>()
+usePageStore().pageData.title = 'Projects'
 
-;(async () => {
-  const { data, error } = await supabase.from('projects').select()
+const projects = ref<Projects | null>()
+
+const getProjects = async () => {
+  const { data, error } = await projectsQuery
   if (error) {
     console.log(error)
   }
 
   projects.value = data
-})()
+}
 
-const columns: ColumnDef<Tables<'projects'>>[] = [
-  {
-    accessorKey: 'name',
-    header: () => h('div', { class: 'text-left' }, 'Name'),
-    cell: ({ row }) => {
-      return h(
-        RouterLink,
-        {
-          to: `/projects/${row.original.slug}`,
-          class: 'text-left font-medium hover:bg-muted block w-full',
-        },
-        () => row.getValue('name'),
-      )
-    },
-  },
-  {
-    accessorKey: 'status',
-    header: () => h('div', { class: 'text-left' }, 'Status'),
-    cell: ({ row }) => {
-      return h(
-        'div',
-        { class: 'text-left font-medium' },
-        row.getValue('status'),
-      )
-    },
-  },
-  {
-    accessorKey: 'collaborators',
-    header: () => h('div', { class: 'text-left' }, 'Collaborators'),
-    cell: ({ row }) => {
-      return h(
-        'div',
-        { class: 'text-left font-medium' },
-        JSON.stringify(row.getValue('collaborators')),
-      )
-    },
-  },
-]
+await getProjects()
 </script>
 
 <template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
 import VueRouter from 'unplugin-vue-router/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
+
+import Components from 'unplugin-vue-components/vite'
 
 import autoprefixer from 'autoprefixer'
 import tailwind from 'tailwindcss'
@@ -11,6 +15,26 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     VueRouter(),
+    Components({}),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/, // .md
+      ],
+      imports: [
+        // presets
+        'vue',
+        VueRouterAutoImports,
+        {
+          pinia: ['defineStore', 'storeToRefs', 'acceptHMRUpdate'],
+        },
+      ],
+      dts: true,
+      viteOptimizeDeps: true,
+      dirs: ['src/stores'],
+    }),
     vue({
       template: {
         compilerOptions: {
